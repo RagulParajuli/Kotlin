bed58ef6a9c4625ca4367cdbbc8e9b0ed677712a
RAGUL <parajuliragul@gmail.com>
2023-11-04 22:38:22 +0530

    Variables

---
+++ src/main/kotlin/variables.kt
@@ -0,0 +1,12 @@
+
+
+fun main() {
+    val name: String = "Ragul"
+    var age: Int = 18
+    var bool: Boolean = true
+    println("My name is $name")
+    println("My age is $age")
+    age = 19
+    println("But in a few days I will turn to $age and It's $bool")
+
+}
