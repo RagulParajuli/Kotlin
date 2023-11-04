ff6948fca4c752cfedbc1a5b8f619ad11d4b128d
RAGUL <parajuliragul@gmail.com>
2023-11-04 22:55:28 +0530

    Operators

---
+++ src/main/kotlin/Area.kt
@@ -0,0 +1,9 @@
+import java.util.*
+
+fun main() {
+    val pi = 3.14F
+     var r = 22.4F
+    val ans = 4/3 * pi * r * r * r
+    println("The Volume of the sphere with the radius $r is : $ans")
+
+}
---
+++ src/main/kotlin/Calculate.kt
@@ -0,0 +1,13 @@
+
+
+fun main() {
+    val x = 20F
+    val y = 7F
+
+    println("The $x + $y : $(x + y)")
+    println("The $x - $y : $(x - y)")
+    println("The $x * $y : $(x * y)")
+    println("The $x / $y : $(x / y)")
+
+
+}
